8a7c00bb185409891f2782ecfa72d8c34346ebd1
Amarius Alla <alla.amarius@gmail.com>
2022-04-26 01:08:31 +0200

    Api Authentication with Personal Access Token

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -26,18 +26,20 @@ class GoogleController extends Controller
             $user = User::where('email', $google_user->email)->first();
             if ($user){
                 Auth::login($user);
-                return view('user',[ 'user' => $user]);
+                $accessToken = Auth::user()->createToken('authToken')->accessToken;
+                return response(['user'=> Auth::user(), 'access_token' => $accessToken]);
             } else {
                 $new_user = User::create([
                     'name' => ucwords($google_user->name),
                     'email' => $google_user->email,
                     'email_verified_at' => now(),
                     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                    'remember_token' => 'test',//Str::random(10),
+                    'remember_token' => Str::random(10),
                 ]);
 
                 Auth::login($new_user);
-                return view('user', ['user' => $new_user]);
+                $accessToken = Auth::user()->createToken('authToken')->accessToken;
+                return response(['user'=> Auth::user(), 'access_token' => $accessToken]);
             }
 
         } catch(\Throwable $th){
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GoogleController;
 use App\Http\Controllers\GithubController;
+use App\Http\Controllers\UserController;
 use Laravel\Socialite\Two\GithubProvider;
 
 /*
@@ -17,9 +18,7 @@ use Laravel\Socialite\Two\GithubProvider;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:api')->get('/user/all', [UserController::class, 'index']);
 
 Route::get('login/google/callback', [GoogleController::class, 'callback']);
 
